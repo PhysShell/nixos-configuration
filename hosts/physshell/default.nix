6c2d46741905f73a474f6143d994b8812ae90a48
PhysShell <nixos@nixos.sector.kz>
2024-08-14 10:24:14 +0000

    restore flakes support

--- hosts/physshell/default.nix
+++ hosts/physshell/default.nix
@@ -19,6 +19,8 @@
     htop
   ];
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
